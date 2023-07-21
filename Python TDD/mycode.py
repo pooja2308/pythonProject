d62e3637af03ed70a85c75610026cb2f3498a8d7
Pooja Pandey <poojapandey2308@gmail.com>
2023-07-21 16:02:49 +0100

    added in tests

---
+++ Python TDD/mycode.py
@@ -0,0 +1,14 @@
+import calendar
+
+def hello_world():
+    return 'Hello World'
+
+def create_num_list(length):
+    return [x for x in range(length)]
+
+def custom_func_x(x, const, power):
+    return const * x **power
+
+def custom_non_lin_num_list(length, const, power):
+    return [custom_func_x(x, const, power) for x in range(length)]
+
